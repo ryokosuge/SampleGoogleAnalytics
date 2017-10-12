5e96108f134489dabc11d7b8012227494eb4ec87
ryokosuge <ryo.kosuge@gmail.com>
2017-10-12 21:36:03 +0900

    first commit.

---
+++ Podfile
@@ -0,0 +1,12 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'SampleGoogleAnalytics' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SampleGoogleAnalytics
+  pod 'GoogleAnalytics', '~> 3.17.0'
+  pod 'GoogleIDFASupport', '~> 3.14.0'
+
+end
---
+++ SampleGoogleAnalytics/Bridging-Header.h
@@ -0,0 +1,18 @@
+//
+//  Bridging-Header.h
+//  SampleGoogleAnalytics
+//
+//  Created by nagisa-kosuge on 2017/10/12.
+//  Copyright © 2017年 RyoKosuge. All rights reserved.
+//
+
+#ifndef SampleGoogleAnalytics_Bridging_Header_h
+#define SampleGoogleAnalytics_Bridging_Header_h
+
+/// GoogleAnalytics
+#import <GoogleAnalytics/GAI.h>
+#import <GoogleAnalytics/GAIFields.h>
+#import <GoogleAnalytics/GAIDictionaryBuilder.h>
+#import <GoogleAnalytics/GAIEcommerceFields.h>
+
+#endif

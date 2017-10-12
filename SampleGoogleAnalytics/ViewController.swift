4b919d2e8b5c3bd5e3697dea99c801d6bfb168d2
ryokosuge <ryo.kosuge@gmail.com>
2017-10-12 23:26:14 +0900

    イベントやスクリーンなどを計測

--- SampleGoogleAnalytics/ViewController.swift
+++ SampleGoogleAnalytics/ViewController.swift
@@ -13,6 +13,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        // GAITrackedViewControllerのtrackerは`GAI.sharedInstance().defaultTracker`が使用されます
+        // 独自のものを使う場合は指定する必要があります
+        // self.tracker = self.tracker()
+
+        // スクリーン名の指定
+        // viewDidAppearのタイミングで送信されるので、それまでにセットしておく
+        // screenNameがnilの場合は送信されない
+        // self.screenName = "トップ"
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        sendScreen()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +34,46 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+}
+
+private let tapCountKey = "customDimensionTapCount"
+
+extension ViewController {
+
+    @IBAction func tapButton(_ button: UIButton) {
+        sendEvent(category: "TOP", action: "タップ", label: "ボタン")
+        let tapCount = UserDefaults.standard.integer(forKey: tapCountKey) + 1
+        UserDefaults.standard.set(tapCount, forKey: tapCountKey)
+    }
+
+    func sendEvent(category: String, action: String, label: String) {
+        let tracker = self.tracker()
+        let params = GAIDictionaryBuilder.createEvent(withCategory: category, action: action, label: label, value: nil).build() as! [AnyHashable: Any]
+        tracker?.send(params)
+    }
 
 }
 
+extension ViewController {
+
+    func sendScreen() {
+        let tracker = self.tracker()
+        tracker?.set(kGAIScreenName, value: "トップ")
+        let params = GAIDictionaryBuilder.createScreenView().build() as! [AnyHashable: Any]
+        tracker?.send(params)
+    }
+
+}
+
+extension ViewController {
+
+    func tracker() -> GAITracker? {
+        let tracker = GAI.sharedInstance().defaultTracker
+        tracker?.allowIDFACollection = true
+        let tapCount = UserDefaults.standard.integer(forKey: tapCountKey)
+        let field = GAIFields.customDimension(for: 1)
+        tracker?.set(field, value: "\(tapCount)")
+        return tracker
+    }
+
+}
